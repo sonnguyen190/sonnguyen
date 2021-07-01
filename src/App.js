6a9fc06cf593b74f6550862f641c470d979c1258
sonnguyen190 <sonkute190@gmail.com>
2021-07-01 19:14:08 +0700

    lên

---
+++ src/App.js
@@ -0,0 +1,43 @@
+import './App.css';
+function App() {
+  return (
+    <div className="App" >
+      <div className='fss'>   <h1> My Profile</h1>
+   <img className="image" src="https://scontent.fpnh22-3.fna.fbcdn.net/v/t1.6435-1/p240x240/121504294_776632179843389_8604437484388416535_n.jpg?_nc_cat=109&ccb=1-3&_nc_sid=7206a8&_nc_ohc=Sf-s2OU58-cAX8XqK5x&tn=MW9RXgUXB6bI-xWs&_nc_ht=scontent.fpnh22-3.fna&tp=6&oh=d4bff732954ec717488be31f6615e9ae&oe=60E24489" alt="" />
+   <h2>Nguyễn Văn Lâm Sơn  </h2>
+   <h4>Lập Trình Viên Cùi Bắp</h4>
+   <img className='gif' src="https://img2.thuthuatphanmem.vn/uploads/2018/12/31/hinh-anh-dong-ngo-nghinh-de-thuong-nhat_112054325.gif" alt="" />
+
+   <div> <h4>Chào Bro.Rất vui vì được làm quen với Bro! <br/>
+    Bro muốn thông tin gì của tui nào?</h4>
+    
+   </div>
+</div>
+
+   <div>
+    <ul className="ulli"> 
+
+      <li>
+     
+      <a className='linkfb' href="https://www.facebook.com/sonvjpro.nguyen/"> <i class="bi bi-facebook"></i>  Facebook</a>
+      </li>
+
+      <li>
+      
+      <a className='linkfb' href="tel:0355047879"><i class="bi bi-telephone-fill"></i>  NumberPhone</a>
+      </li>
+
+      <li>
+      
+      <a className='linkfb' href="mailto:sonkute190@gmail.com"><i class="bi bi-envelope-fill"></i>  Gmail</a>
+      </li>
+
+    </ul>
+   </div>
+   © 2021 Namecard | Created by <a className='linkfb1' href="https://www.facebook.com/sonvjpro.nguyen">Sơn Nguyễn</a> 
+    </div>
+    
+  );
+}
+
+export default App;
