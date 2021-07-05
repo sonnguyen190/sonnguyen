fe8b5236f98de5ab60c912a33b34bd9a464d978b
sonnguyen190 <sonkute190@gmail.com>
2021-07-05 23:58:13 +0700

    điền nội dung commit vào đây

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ function App() {
    <img className="image" src="https://scontent.fpnh22-3.fna.fbcdn.net/v/t1.6435-9/121504294_776632179843389_8604437484388416535_n.jpg?_nc_cat=109&ccb=1-3&_nc_sid=09cbfe&_nc_ohc=RkYBw-WzFY8AX8VBgL4&tn=MW9RXgUXB6bI-xWs&_nc_ht=scontent.fpnh22-3.fna&oh=d4f6426b5b4b357f9f3547653aecddb1&oe=60E729EC" alt="" />
    <h2>Nguyễn Văn Lâm Sơn  </h2>
    <h4>Lập Trình Viên Cùi Bắp</h4>
-   <img className='gif' src="https://img2.thuthuatphanmem.vn/uploads/2018/12/31/hinh-anh-dong-ngo-nghinh-de-thuong-nhat_112054325.gif" alt="" />
+   <img className='gif' src="https://img2.thuthuatphanmem.vn/uploads/2018/12/31/anh-dong-de-thuong-va-dep_112053878.gif" alt="" />
 
    <div> <h4>Chào Bro.Rất vui vì được làm quen với Bro! <br/>
     Bro muốn thông tin gì của tui nào?</h4>
@@ -18,18 +18,18 @@ function App() {
     <ul className="ulli"> 
 
       <li>
-     
-      <a className='linkfb' href="https://www.facebook.com/sonvjpro.nguyen/"> <i class="bi bi-facebook"></i>  Facebook</a>
+     <button className='button'><a className='linkfb' href="https://www.facebook.com/sonvjpro.nguyen/"> <i class="bi bi-facebook"></i>  Facebook</a></button>
+      
       </li>
 
       <li>
+      <button className='button'><a className='linkphone' href="tel:0355047879"><i class="bi bi-telephone-fill"></i> Phone</a></button>
       
-      <a className='linkfb' href="tel:0355047879"><i class="bi bi-telephone-fill"></i>  NumberPhone</a>
       </li>
 
       <li>
+      <button className='button'><a className='linkgmail' href="mailto:sonkute190@gmail.com"><i class="bi bi-envelope-fill"></i>  Gmail</a></button>
       
-      <a className='linkfb' href="mailto:sonkute190@gmail.com"><i class="bi bi-envelope-fill"></i>  Gmail</a>
       </li>
 
     </ul>
